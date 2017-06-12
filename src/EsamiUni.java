08aacdd4d3374048c4396e437d5f69b888126f54
Mauriliaquinci <macrivifla@gmail.com>
2017-06-12 11:17:59 +0200

    Add files via upload

---
+++ src/EsamiUni.java
@@ -0,0 +1,314 @@
+package prenotazioneesami;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @version 1.0 28.06.2017
+ * @author Maurilia Quinci
+ *
+ */
+/**
+ * Nel Menù Principale è possibile accedere in due aree: 
+ * >Studente ->Prenotazione esami
+ * 			 ->Visualizza esami prenotati
+ * >Docente  ->Visualizza prenotazioni-insegnamento
+ */
+
+
+public class EsamiUni  {
+	
+	static Sistema sistema = new Sistema();
+	static ArrayList<Integer> conta_accesso= new ArrayList<Integer>();
+	static boolean accesso_studente;
+	static boolean accesso_docente;
+	static int intero=0;
+	static Scanner input= new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException {
+			
+			ArrayList<CarrieraStudente> carr_studente= new ArrayList<CarrieraStudente>();
+		    String line;
+		    String codice, nome, cfu, appello;
+		    
+		    FileReader f;
+			f=new FileReader("file/carriereStudenti");
+		    BufferedReader bufferInsegn;
+		    bufferInsegn=new BufferedReader(f);
+		    
+		    line=bufferInsegn.readLine();
+		   	/*Lettura file Insegnamenti cosi organizzato:
+		   	 * matricola
+		   	 * n numero insegnamenti prenotabili
+		   	 * n righe che descrivono un insegnamento composte da:
+		   	 * 	Codice Nome CFU Appello1 Appello2 Appello3
+		   	 * */	   
+		    while(line!=null) {
+		    	
+		    	ArrayList<Insegnamento> materie= new ArrayList<Insegnamento>();
+		    	Studente st_iesimo = new Studente();
+		    	CarrieraStudente c = new CarrieraStudente();
+		    	
+		    	st_iesimo.setMatricola(line); 
+		    	line=bufferInsegn.readLine(); 
+		    	int num= Integer.parseInt(line);
+		    	line=bufferInsegn.readLine();
+		    	
+		    	for (int i=0; i<num; i++) {
+		    		ArrayList<String> appel = new ArrayList<String>();
+		    		Insegnamento ins = new Insegnamento(null,null,null,null, null, null, null);
+		    		StringTokenizer st = new StringTokenizer(line); //divido in token la riga
+		    		
+		    		while (st.hasMoreTokens()) {
+		    			codice=st.nextToken();
+		    			ins.setCodice(codice);
+		    			nome=st.nextToken();
+		    			ins.setNome(nome);
+		    			cfu=st.nextToken();
+		    			ins.setCfu(cfu);
+		    			
+		    			for (int h=0; h<3; h++) {
+		    				appello=st.nextToken();
+		    				appel.add(appello);
+		    			}
+		    			
+		    			ins.setAppello(appel);
+		    			materie.add(ins);
+		    		}
+		    		line=bufferInsegn.readLine(); 
+		    	}	
+		    	c.setListaInsegnamento(materie);    
+		    	c.setStudente(st_iesimo);
+		    	carr_studente.add(c);			       
+		    }
+		    
+		    bufferInsegn.close();
+		     
+		    /*Richiamo la funzione dalla classe sistema per settare le carriere di ogni studente*/
+		    sistema.setCarrStud(carr_studente); 
+		    /*Richiamo la funzione utentiRegistrati per memorizzare la lista degli utenti registrati*/
+		    sistema.utentiRegistrati();
+		  
+		    menuPrincipale();
+		    System.out.println("");
+		    System.out.println("----------------- BENVENUTO ---------------");
+	}
+	
+	
+	
+	@SuppressWarnings({ "unused", "resource" })
+	public static void menuPrincipale() throws IOException {
+
+		System.out.println("");
+	    System.out.println("---------------MENU PRINCIPALE---------------");
+	    System.out.println();
+	    System.out.println("  "+ "s/S - Studente");
+	    System.out.println("  "+ "d/D - Docente");
+	    System.out.println("  "+ "l/L - Logout");
+		
+		UtenteRegistrato uten = new UtenteRegistrato();
+		Scanner i = new Scanner(System.in);
+		char studente_docente='x';
+	
+	    do {
+	    	studente_docente = i.next().charAt(0);
+	    	
+	    	switch (studente_docente) {
+			case 's': 
+				/*Se l'accesso viene effettuato da uno studente dopo che è stato effettuato 
+				 * da un docente eseguo il logout e accedo come studente*/
+			    if (accesso_docente==true) {
+			    	sistema.logout();
+		    		accesso_docente=false;
+		    	}
+			    	
+		    	sistema.login(true); 
+		    	
+		    	if (accesso_studente==false) {
+		    		accesso_studente=true; 	
+		    	}
+		    	
+		    	/*Controllo se il login è avvenuto con successo 
+		    	 * in caso contrario ritorno al menuPrincipale*/
+		    	if (sistema.login(accesso_studente)==1) {
+		    		menuPrincipale(); 
+		    	} else { 
+		    		menuPrincipaleStudente();
+		    	}
+		    	break;
+		   				
+		    case 'S':
+		    	if (accesso_docente==true) {
+					sistema.logout();
+					accesso_docente=false;
+		    	}
+		    	
+		    	sistema.login(true);// accedo come studente
+				
+		    	if (accesso_studente==false) {
+		    		accesso_studente=true; 	
+		    	}
+		    	
+				if (sistema.login(accesso_studente)==1) {
+					menuPrincipale();
+				} else {
+					menuPrincipaleStudente();
+				}
+				break;
+					    				
+		    case 'd': 
+		    	/*Se l'accesso viene effettuato da un docente dopo che è stato effettuato 
+				 * da uno studente eseguo il logout e accedo come docente*/
+		    	if (accesso_studente==true) {
+					sistema.logout();
+					accesso_studente=false;
+				}
+		    	
+		    	System.out.println();
+		    	sistema.login(false); // accedo come docente
+		    	if (accesso_docente==false) {
+		    		accesso_docente=true;
+		    	}
+		    						
+		    	if (sistema.login(accesso_docente)==1) {
+		    		menuPrincipale();
+		    	} else {
+		    		menuPrincipaleDocente();
+		    	}
+		    	break;
+		    				
+		    case 'D':  
+		    	if (accesso_studente==true) {
+					sistema.logout();
+					accesso_studente=false;
+		    	}
+		    
+		    	System.out.println();
+		    			
+		    	sistema.login(false); // accedo come docente
+		    	if (accesso_docente==false) {
+		    		accesso_docente=true;
+		    	}
+		    	if (sistema.login(accesso_docente)==1) {
+		    		menuPrincipale();
+		    	} else {
+		    		menuPrincipaleDocente();
+				}
+		    	break;	
+		    			
+		    case 'l': 	
+		    	sistema.logout();
+		    	menuPrincipale();
+		    	break;
+		    				
+		    case 'L': 
+		    	sistema.logout();
+				menuPrincipale();
+				break;
+						
+		    default: 	
+		    	System.out.println();
+				System.out.println("# Input non corretto #");
+				menuPrincipale();
+				break;
+	    	}
+	    	
+	    } while (studente_docente!='x');
+		
+	}
+	
+	public static void menuPrincipaleStudente() throws IOException {
+	
+		
+		System.out.println();
+		System.out.println("---------->Effettua una scelta");
+		System.out.println();
+		System.out.println("  "+ "1 - Prenotazione Esami");
+		System.out.println("  "+ "2 - Visualizza Esami prenotati");
+		System.out.println("  "+ "0 - Ritorna al Menu Principale");
+
+		String carattere = input.nextLine();
+		try {
+			do {
+			
+				intero= Integer.parseInt(carattere);
+			
+				switch (intero) {
+				case 0:	
+					System.out.println("");
+					menuPrincipale();
+					break;
+		    	
+				case 1: 
+					System.out.println();
+					sistema.prenotazione();			
+					break;
+					
+				case 2:
+					System.out.println();
+					sistema.visualizzaPrenotazioni();
+					break;
+	    		
+				case 3:	
+					menuPrincipaleStudente();
+					break;
+	  
+				default: 
+					System.out.println();
+					System.out.println("# Input non corretto #");
+					menuPrincipaleStudente();
+					break;
+				}
+			} while (intero!=0);
+		} catch (Exception e) {
+			System.out.println("Input inserito non è un numero");
+		}
+		
+	}
+	
+	
+	public static void menuPrincipaleDocente() throws IOException{
+		
+		System.out.println();
+		System.out.println("---------->PROCEDI");
+		System.out.println();
+		System.out.println("  "+ "1 - Visualizza Prenotazioni-Insegnamento");
+		System.out.println("  "+ "0 - Ritorna al Menu Principale");
+		System.out.println();
+		
+		String valor= input.nextLine();
+		try {
+			do{
+				intero= Integer.parseInt(valor);
+			
+				switch(intero){
+				case 0:	
+					System.out.println("");
+					menuPrincipale();
+					break;
+	
+				case 1: 
+					sistema.visualizzaPrenotazioniInsegnamento();			
+					break;
+	    
+				default:
+					System.out.println();
+					System.out.println("# Input non corretto #");
+					menuPrincipaleDocente();
+					break;
+				}
+			} while (intero!=0);
+		} catch (Exception e) {
+			System.out.println("Input inserito non è un numero");
+		}
+	}
+		
+}
+
+
---
+++ src/Insegnamento.java
@@ -0,0 +1,190 @@
+package prenotazioneesami;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @version 1.0 28.06.2017
+ * @author Maurilia Quinci
+ *
+ */
+
+/**
+ * Specifica il codice, nome, numero di crediti formativi
+ * classe(primo, secondo...quinto anno)di un insegnamento e la lista degli appelli per sostenere un esame.
+ * Ad ogni insegnamento corrisponde una o più prenotazioni
+ */
+
+public class Insegnamento {
+	
+    /** Attributes */
+    private String codice;
+    private String nome;
+    private String cfu;
+    private String classe;
+    private ArrayList<String> appello;
+    
+    /** Associations */
+    private Esame esame;
+    private ArrayList<Prenotazione> prenotazione;
+   
+    /**Costruttore 
+     * 
+     * @param codice
+     * @param nome
+     * @param cfu
+     * @param classe
+     * @param appello
+     * @param esame
+     * @param prenotazione
+     */
+    public Insegnamento(String codice, String nome, String cfu, String classe,
+    					ArrayList<String> appello, Esame esame, 
+    					ArrayList<Prenotazione> prenotazione ) {
+    	this.codice=codice;
+    	this.nome=nome;
+    	this.cfu=cfu;
+    	this.classe=classe;
+    	this.esame=esame;
+    	prenotazione=new ArrayList<Prenotazione>();
+    	appello= new ArrayList<String>();
+    }
+    
+    /**Costruttore senza parametri
+     * 
+     */
+    public Insegnamento() {}
+    
+    /**Operazione getCodice
+     * 
+     * @return codice
+     */
+    public String getCodice() {
+    	return codice;
+    }
+    
+    /**Operazione setCodice
+     * 
+     * @param codice
+     */
+    public void setCodice(String codice) {
+    	this.codice=codice;
+    }
+    
+    /**Operazione getNome
+     * 
+     * @return nome
+     */
+    public String getNome() {
+    	return nome;
+    }
+    
+    /**Operazione setNome
+     * 
+     * @param nome
+     */
+    public void setNome(String nome) {
+    	this.nome=nome;
+    }
+    
+    /**Operazione getCfu
+     * 
+     * @return cfu
+     */
+    public String getCfu() {
+    	return cfu;
+    }
+    
+    /**Operazione setCfu
+     * 
+     * @param cfu
+     */
+    public void setCfu(String cfu) {
+    	this.cfu=cfu;
+    }
+    
+    /**Operazione getClasse
+     * 
+     * @return classe
+     */
+    public String getClasse() {
+    	return classe;
+    }
+    
+    /**Operazione setClasse
+     * 
+     * @param classe
+     */
+    public void setClasse(String classe) {
+    	this.classe=classe;
+    }
+    
+    /**Operazione getEsame
+     * 
+     * @return esame
+     */
+    public Esame getEsame() {
+    	return esame;
+    }
+    
+    /**Operazione setEsame
+     * 
+     * @param esame
+     */
+    public void setEsame(Esame esame) {
+    	this.esame=esame;
+    }
+    
+    /**Operazione getPrenotazione
+     * 
+     * @return prenotazione
+     */
+    public  ArrayList<Prenotazione> getPrenotazione() {
+    	return prenotazione;
+    }
+    
+    /**Operazione setPrenotazione
+     * 
+     * @param prenotazione
+     */
+    public void setPrenoazione( ArrayList<Prenotazione> prenotazione) {
+    	this.prenotazione=prenotazione;
+    }
+    
+    /**Operazione getAppello
+     * 
+     * @return appello
+     */
+    public ArrayList<String> getAppello() {
+    	return appello;
+    }
+    
+    /**Operazione setAppello
+     * 
+     * @param appello
+     */
+    public void setAppello(ArrayList<String> appello) {
+    	this.appello=appello;
+    }
+    /**
+     * Operation addAppello
+     *
+     * @param data 
+     * @return void
+     */
+    public void addAppello(String data ) {
+    	if(appello.size()<4){
+    		appello.add(data);
+    	}
+    }
+    
+  
+    
+    
+    
+    
+    
+    
+  
+}
+
